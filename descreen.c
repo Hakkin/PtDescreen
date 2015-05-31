820c92253a958d94cae1ea06c5a78cf2969f4548
Hakkin Lain <Hakkin@github>
2015-05-30 20:58:46 -0700

    Added image reading and writing code, also fixed using main() in descreen.c

--- descreen.c
+++ descreen.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int descreen()
 {
     return 0;
 }
--- main.c
+++ main.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_FAILURE_USERMSG
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+int main(int argc, char *argv[])
 {
+    if (argc < 4)
+    {
+        printf("Usage: %s [input] [output] [DPI]\n", argv[0]);
+        return 0;
+    }
+
+    printf("Reading input image...");
+    int width, height, comp;
+    // The final argument to this specifies the number of components to return,
+    // we will always be working with 3 (Red, Green and Blue)
+    // comp will be the original number of components  in the image
+    // It might be better to request 4 components here (RGBA) so we cant't throw away any information
+    unsigned char *pixels = stbi_load(argv[1], &width, &height, &comp, 3);
+    if (pixels == NULL)
+    {
+        printf("\nError reading image %s: %s", argv[1], stbi_failure_reason());
+    }
+
+    // Descreen image
+
+    printf("\nWriting output image...");
+    // We will always output 24bit PNG for now, regardless of output extension
+    stbi_write_png(argv[2], width, height, 3, pixels, 0);
+
     return 0;
 }
