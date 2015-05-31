5e1e057e06d6e35f86a5bc1a385b910c5908c387
Hakkin Lain <Hakkin@github>
2015-05-31 09:48:04 -0700

    Added initial function prototypes & config struct These will likely change later, but they will probably work for now.

--- descreen.c
+++ descreen.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "descreen.h"
 
-int descreen()
+int analyze(descreenConfig *config, unsigned int x, unsigned int y, unsigned int pow2)
+{
+    return 0;
+}
+
+int descreen(descreenConfig *config, unsigned int pow2)
 {
     return 0;
 }
--- descreen.h
+++ descreen.h
@@ -1,6 +1,25 @@
 #ifndef DESCREEN_H_INCLUDED
 #define DESCREEN_H_INCLUDED
 
+typedef struct
+{
+    char *pixels;
+    int width;
+    int height;
+    int dpi;
+    int lpi;
+    int angle;
 
+} descreenConfig;
+
+// analyze() will analyze a 2^pow2 sized square at (x, y) in *pixels,
+// if it detects a screentone, it will set lpi and angle in *config
+// to the detected values and will return a non-zero value.
+// If no screentone could be detected, it will return 0 and *config will be unmodified.
+int analyze(descreenConfig *config, unsigned int x, unsigned int y, unsigned int pow2);
+
+// descreen() will apply a descreen filter to *pixels using the
+// parameters provided in *config, using a 2^pow2 sized square window.
+int descreen(descreenConfig *config, unsigned int pow2);
 
 #endif // DESCREEN_H_INCLUDED
