2d389b17145388727cf47886e59e093c547d8fe4
Hakkin Lain <Hakkin@github>
2015-06-03 11:40:42 -0700

    Added functions to generate magnitude and find peaks. The peak-finding function could probably be cleaner, but it works for now.

--- descreen.c
+++ descreen.c
@@ -5,6 +5,12 @@
 #include <fftw3.h>
 #include "descreen.h"
 
+// Generates magnitude value from a real and imaginary value scaled by the scale value
+double genMagnitude(double real, double imag, double scale);
+// Finds peaks in magnitude by comparing all 4 pixels around it, if the specified
+// pixel is a peak, it will return a non-zero value, otherwise it will return 0
+int isPeak(unsigned int width, unsigned int height, fftw_complex *fft, unsigned int x, unsigned int y);
+
 int analyze(descreenConfig *config, unsigned int x, unsigned int y, unsigned int pow2)
 {
     unsigned int analyzeSize = pow(2, pow2);
@@ -55,4 +61,77 @@ int analyze(descreenConfig *config, unsigned int x, unsigned int y, unsigned int
 int descreen(descreenConfig *config, unsigned int pow2)
 {
     return 0;
+}
+
+double genMagnitude(double real, double imag, double scale)
+{
+    real = real/scale;
+    imag = imag/scale;
+    return sqrt((real*real)+(imag*imag));
+}
+
+int isPeak(unsigned int width, unsigned int height, fftw_complex *fft, unsigned int x, unsigned int y)
+{
+    double scale = width*height;
+    double magValue = genMagnitude(fft[y*width+x][0], fft[y*width+x][1], scale);
+
+    int isPeak = 1;
+    // Checks each pixel around the specified one to check if it's brighter,
+    // if it is, isPeak is set to 0 and we will break from the loop
+    // TODO: There is probably a better way to do this, but this works for now
+    for (unsigned int side = 0; side < 4; side++)
+    {
+        switch (side)
+        {
+            case 0:
+                if (y == 0)
+                {
+                    break;
+                }
+                if (magValue <= genMagnitude(fft[(y-1)*width+x][0], fft[(y-1)*width+x][1], scale))
+                {
+                    isPeak = 0;
+                }
+                break;
+            case 1:
+                if (x == width-1)
+                {
+                    break;
+                }
+                if (magValue <= genMagnitude(fft[y*width+(x+1)][0], fft[y*width+(x+1)][1], scale))
+                {
+                    isPeak = 0;
+                }
+                break;
+            case 2:
+                if (y == height-1)
+                {
+                    break;
+                }
+                if (magValue <= genMagnitude(fft[(y+1)*width+x][0], fft[(y+1)*width+x][1], scale))
+                {
+                    isPeak = 0;
+                }
+                break;
+            case 3:
+                if (x == 0)
+                {
+                    break;
+                }
+                if (magValue <= genMagnitude(fft[y*width+(x-1)][0], fft[y*width+(x-1)][1], scale))
+                {
+                    isPeak = 0;
+                }
+                break;
+            default:
+                // This should never happen
+                return 0;
+        }
+        if (isPeak == 0)
+        {
+            break;
+        }
+    }
+
+    return isPeak;
 }
